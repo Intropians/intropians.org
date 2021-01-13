23762f087bde5caf169c7e3c15756aac6659068a
Steve Linux <sfarrugia@gmail.com>
2021-01-13 08:44:24 +0100

    New branch for experimental features

--- js/settings/personalCustomisers.js
+++ js/settings/personalCustomisers.js
@@ -3,26 +3,26 @@ const personalCustomisers = [
         name: 'theme',
         defaultVal: 'auto'
     },
-    // {
-    //     name: 'link-visibility',
-    //     defaultVal: 'visible'
-    // },
-    // {
-    //     name: 'text-alignment',
-    //     defaultVal: 'left'
-    // },
-    // {
-    //     name: 'font-family',
-    //     defaultVal: 'serif'
-    // },
-    // {
-    //     name: 'text-size',
-    //     defaultVal: 'normal'
-    // },
-    // {
-    //     name: 'text-line-height',
-    //     defaultVal: 'normal'
-    // }
+    {
+        name: 'link-visibility',
+        defaultVal: 'visible'
+    },
+    {
+        name: 'text-alignment',
+        defaultVal: 'left'
+    },
+    {
+        name: 'font-family',
+        defaultVal: 'serif'
+    },
+    {
+        name: 'text-size',
+        defaultVal: 'normal'
+    },
+    {
+        name: 'text-line-height',
+        defaultVal: 'normal'
+    }
 ];
 
 export default personalCustomisers;
